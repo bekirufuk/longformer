eecc954239c29da39dcb224714392ef6d0b90a28
bekirufuk <bekirufuk@gmail.com>
2022-03-01 14:02:06 +0300

    Functions moved to the utils.py file.

--- experiments/config.py
+++ experiments/config.py
@@ -1,26 +1,12 @@
-from transformers import TrainingArguments
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-
 seed = 42
 
 labels_list = ["A","B","C","D","E","F","G","H"]
 num_labels = 8
 
-batch_size = 4
+batch_size = 2#4
 max_length = 4096 #16384
 
 lr= 5e-5
+weight_decay=1e-2
 num_warmup_steps=0
 num_epochs = 1
-
-
-def compute_metrics(predictions, references):
-
-    precision, recall, f1, _ = precision_recall_fscore_support(references, predictions, average='micro')
-    acc = accuracy_score(references, predictions)
-    return {
-        'accuracy': acc,
-        'f1': f1,
-        'precision': precision,
-        'recall': recall
-    }
