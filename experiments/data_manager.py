319f15350edd021ae8f9fe99d8a6b0551efd1682
bekirufuk <bekirufuk@gmail.com>
2022-03-02 00:52:27 +0300

    Initial commit

--- experiments/data_manager.py
+++ experiments/data_manager.py
@@ -1,4 +1,78 @@
+import os
+import json
+import config
 import pandas as pd
 
-df = pd.read_csv('data/patentsview/example/train.csv', usecols=['text', 'label'])
-df.to_csv('data/patentsview/example/train.csv', index=False)
+
+def create_data_files(patents, ipcr):
+    chunk_count = 0
+    patent_count = 0
+    for chunk in patents:
+        # Combine patent with respective section info.
+        data = chunk.merge(ipcr, how='left', on='patent_id')
+
+        # Replace the letters with integers to create a suitable training input.
+        data.replace({'section':config.label2id}, inplace=True)
+
+        # Append the batch to the main data file.
+        data.to_csv(os.path.join(config.data_dir, 'patents_'+config.patents_year+'.csv'),
+            sep=',',
+            mode='a',
+            index=False,
+            columns=['text', 'section'],
+            header = ['text','label']
+        )
+
+        # Seperately write the batches as individual files. (optional)
+        data.to_csv(os.path.join(config.data_dir, 'chunks/patents_'+config.patents_year+'_chunk_'+str(chunk_count).zfill(6)+'.csv'),
+            sep=',',
+            mode='w',
+            index=False,
+            columns=['text', 'section'],
+            header = ['text','label']
+        )
+
+        patent_count += data.shape[0]
+        chunk_count += 1
+        print("Chunk {0} -> Total processed patent count: {1}".format(chunk_count, patent_count))
+
+    # Write the basic info about process data for ease of use.
+    with open(os.path.join(config.data_dir, "meta/patents_"+config.patents_year+"_meta.json"), "w") as f:
+        f.write(json.dumps({"num_chunks":chunk_count,
+                            "chunk_size":config.chunk_size,
+                            "num_patents":patent_count
+                            }))
+
+
+if __name__ == '__main__':
+
+    # Icpr file holds detailed class information about the patents.
+    # We will only investigate section column which consist of 8 distinct classes.
+    ipcr = pd.read_csv(os.path.join(config.data_dir, 'ipcr.tsv'),
+        sep="\t",
+        usecols=['patent_id','section'],
+        dtype={'patent_id':object, 'section':object},
+        engine='c',
+        )
+    print("Ipcr data loaded.")
+
+    # All patents from asinge year chunked. Multiple year processing will be implemented in future.
+    patents = pd.read_csv(os.path.join(config.data_dir, 'detail_desc_text_'+config.patents_year+'.tsv'),
+        sep="\t",
+        usecols=['patent_id', 'text'],
+        dtype={'patent_id':object, 'text':object},
+        engine='c',
+        nrows=300,
+        chunksize=config.chunk_size,
+        )
+    print("Patents data chunked with chunk_size={}.".format(config.chunk_size))
+
+    # Drop duplicates because this table might have duplicating patent_id sharing the same section with different subclasses.
+    ipcr = ipcr.drop_duplicates(subset=['patent_id'])
+    print("Ipcr data de-duplicated.")
+
+    print("\n----------\n DATA PROCESSING STARTED \n----------\n")
+
+    create_data_files(patents, ipcr)
+
+    print("\n----------\n DATA PROCESSING FINISHED \n----------\n")
