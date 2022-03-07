3a4d37ed0bed4ff725604763b2b073fd423a70ee
bekirufuk <bekirufuk@gmail.com>
2022-03-07 12:50:04 +0300

    Add data processing parameters and rearrange.

--- experiments/config.py
+++ experiments/config.py
@@ -8,17 +8,14 @@ data_dir = os.path.expanduser('data/patentsview')
 root_dir = os.path.expanduser("experiments")
 patents_year="2019"
 
-# Tokenizer parameters.
-max_length = 4096 #16384
-load_saved_tokens = False
-save_tokens = True
-
 # Model parameters.
 label2id= {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
 labels_list = ["A","B","C","D","E","F","G","H"]
 num_labels = 8
+
 small_scale = True
-save_model=True
+save_model=False
+load_local_checkpoint = False
 
 # Training parameters.
 batch_size = 2#4
@@ -27,6 +24,17 @@ weight_decay=1e-2
 num_warmup_steps=0
 num_epochs = 1
 
-# Data preprocessing parameters.
-chunk_size = 100
+# Tokenizer parameters.
+max_length = 4096 #16384
+load_saved_tokens = True
+save_tokens = False
 
+# Data preprocessing parameters.
+chunk_size = 10000
+single_chunk = False
+
+upload_to_hf = False
+upload_repo_name = 'ufukhaman/uspto_patents_2019'
+
+download_from_hf = False
+download_repo_name = 'ufukhaman/uspto_patents_2019'
